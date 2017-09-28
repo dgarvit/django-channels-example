45bb1577e14834d740091b01cf9deaed52d4e812
Garvit <d.garvit@gmail.com>
2017-09-28 08:30:28 +0530

    Update consumers, add model LoggedInUser

--- example/models.py
+++ example/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class LoggedInUser(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='logged_in_user')
