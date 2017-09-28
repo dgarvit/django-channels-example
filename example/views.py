7fe1fdd3080678142d31545533767f248e4310fb
Garvit <d.garvit@gmail.com>
2017-09-28 07:59:25 +0530

    Add views

--- example/views.py
+++ example/views.py
@@ -1,6 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-from django.shortcuts import render
+def user_list(request):
+	return render(request, 'example/user_list.html')
 
-# Create your views here.
+
+def log_in(request):
+	form = AuthenticationForm()
+	if request.method == 'POST':
+		form = AuthenticationForm(data=request.POST)
+		if form.is_valid():
+			login(request, form.get_user())
+			return redirect(reverse('example:user_list'))
+		else:
+			print(form.errors)
+	return render(request, 'example/log_in.html', {'form': form})
+
+
+def log_out(request):
+	logout(request)
+	return redirect(reverse('example:log_in'))
+
+
+def sign_up(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('example:log_in'))
+        else:
+            print(form.errors)
+    return render(request, 'example/sign_up.html', {'form': form})
